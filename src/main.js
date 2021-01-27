8418b57854aee2da529e1895df21f936b2b602bc
Snorre Aas <saa@24sevenoffice.com>
2021-01-27 10:15:12 +0100

    feat: installed tailwind, and setup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { createApp } from 'vue'
+import './index.css'
 
+createApp(App).mount('#app')
 Vue.config.productionTip = false
 
 new Vue({
